d0ca484515b232cda4abb4a6cbe311f7eac53cb2
HX-FMino <fmino@soyhenry.com>
2024-03-26 12:04:28 -0300

    first commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateProductDto {
+  
+  @IsString()
+  name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Type(() => Number)
+  @IsPositive()
+  price: number;
+
+}
